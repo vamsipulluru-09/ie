684726d5e648a9f5a88f9ea157ec390f9bf4dcc5
Krishna vamsi <vamsipulluru2004@gmail.com>
2025-04-08 08:43:09 +0530

    iot push

--- test.c
+++ test.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-// These are special numbers used in our magic math (called CRC)
 #define POLYNOMIAL 0xD8
 #define TOP_BIT 0x80
 
-// This function creates a "magic number" (called CRC) from a message
 unsigned char calculate_crc(unsigned char *message, size_t length) {
     unsigned char result = 0;
 
-    // Go through every letter in the message
     for (size_t i = 0; i < length; ++i) {
-        result ^= message[i];  // Mix the letter with the result
+        result ^= message[i];
 
-        // Do 8 steps of magic math for each letter
         for (unsigned char bit = 8; bit > 0; --bit) {
             if (result & TOP_BIT) {
-                result = (result << 1) ^ POLYNOMIAL; // If top bit is 1, mix with polynomial
+                result = (result << 1) ^ POLYNOMIAL;
             } else {
-                result = result << 1; // Just shift left
+                result = result << 1;
             }
         }
     }
 
-    return result; // This is our final magic number (CRC)
+    return result;
 }
 
 int main() {
-    unsigned char message[] = "Hello, CRC!"; // This is our message
-    unsigned char crc_result = calculate_crc(message, strlen((char *)message)); // Calculate CRC
+    unsigned char message[] = "Hello, CRC!";
+    unsigned char crc_result = calculate_crc(message, strlen((char *)message));
 
-    printf("Message: %s\n", message); // Show the message
-    printf("CRC (Magic Number): 0x%02X\n", crc_result); // Show the CRC in hex format
+    printf("Message: %s\n", message);
+    printf("CRC (Magic Number): 0x%02X\n", crc_result);
 
     return 0;
 }
